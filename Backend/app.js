8af64cb7c96664dc1a19560ccf1e03f4f689badb
Raj pal <128685582+Rajpal1905@users.noreply.github.com>
2025-10-03 17:51:00 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express()
  
-app.use(cors ({origin:"http://localhost:5173" , credentials:true}))
+app.use(cors ({origin:"https://aiden-bot.vercel.app" , credentials:true}))
 app.use(cookieParser(process.env.COOKIE_SECRET))
 
 app.use(express.json()) 
